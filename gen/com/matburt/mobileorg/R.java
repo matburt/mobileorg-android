bca957e05e5412c86e2d5a920241a4b0d1b1a562
Matthew Jones <mat@matburt.net>
2009-12-08 02:03:47 -0500

    Now setting configuration in a sqlite database

--- gen/com/matburt/mobileorg/R.java
+++ gen/com/matburt/mobileorg/R.java
@@ -13,8 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int settingsSave=0x7f050003;
+        public static final int webPass=0x7f050002;
+        public static final int webURL=0x7f050000;
+        public static final int webUser=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int settings=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/com/matburt/mobileorg/SettingsActivity.java
+++ src/com/matburt/mobileorg/SettingsActivity.java
@@ -1,14 +1,89 @@
 package com.matburt.mobileorg;
 
+import java.util.HashMap;
+import java.util.Map;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 
-public class SettingsActivity extends Activity
+public class SettingsActivity extends Activity implements OnClickListener
 {
+    private Map<String, String> settings;
+    private EditText webUrl;
+    private EditText webUser;
+    private EditText webPass;
+    private Button saveButton;
+
+    private SQLiteDatabase appdb;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        this.settings = new HashMap<String, String>();
+        this.webUrl = (EditText)this.findViewById(R.id.webURL);
+        this.webUser = (EditText)this.findViewById(R.id.webUser);
+        this.webPass = (EditText)this.findViewById(R.id.webPass);
+        this.saveButton = (Button)this.findViewById(R.id.settingsSave);
+        this.saveButton.setOnClickListener(this);
+        this.initializeSettings();
     }
 
+    public void initializeSettings() {
+        this.appdb = this.openOrCreateDatabase("DatabaseName",
+                                               MODE_PRIVATE, null);
+        this.appdb.execSQL("CREATE TABLE IF NOT EXISTS settings"
+                           + " (key VARCHAR, val VARCHAR);");
+        Cursor result = this.appdb.rawQuery("SELECT *" +
+                                         " FROM settings",
+                                         null);
+        this.settings.clear();
+        if (result != null) {
+            if (result.getCount() > 0) {
+                result.moveToFirst();
+                do {
+                    this.settings.put(result.getString(0),
+                                      result.getString(1));
+                } while (result.moveToNext());
+                this.populateDisplay();
+            }
+            else {
+                this.appdb.execSQL("INSERT INTO settings (key, val)" +
+                                   " VALUES ('webUrl', '')");
+                this.appdb.execSQL("INSERT INTO settings (key, val)" +
+                            " VALUES ('webUser', '')");
+                this.appdb.execSQL("INSERT INTO settings (key, val)" +
+                            " VALUES ('webPass', '')");
+            }
+        }
+        this.appdb.close();
+    }
+
+    public void populateDisplay() {
+        this.webUrl.setText(this.settings.get("webUrl"));
+        this.webUser.setText(this.settings.get("webUser"));
+        this.webPass.setText(this.settings.get("webPass"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        this.onSave();
+    }
+
+    public void onSave() {
+        this.appdb = this.openOrCreateDatabase("DatabaseName",
+                                               MODE_PRIVATE, null);
+        this.appdb.execSQL("UPDATE settings set val = '" + this.webUrl.getText() + "' where key = 'webUrl'");
+        this.appdb.execSQL("UPDATE settings set val = '" + this.webUser.getText() + "' where key = 'webUser'");
+        this.appdb.execSQL("UPDATE settings set val = '" + this.webPass.getText() + "' where key - 'webPass'");
+        this.appdb.close();
+        this.finish();
+    }
 }
