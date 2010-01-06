768e81ab67a2cd2da19a96f1532789e62fbea17b
Matthew Jones <mat@matburt.net>
2010-01-06 01:24:27 -0500

    Filling in more Node elements, this will be somewhat of a wrapper around the arraylist and payloads

--- src/com/matburt/mobileorg/Node.java
+++ src/com/matburt/mobileorg/Node.java
@@ -5,14 +5,28 @@ import java.util.ArrayList;
 class Node {
 
     public enum NodeType {
-        HEADING, NONHEADING
+        HEADER, HEADING, COMMENT, DATA
     }
 
     String nodeName = "";
     NodeType nodeType;
     ArrayList<Node> subNodes;
+    String nodePayload = "";
 
-    Node(String heading) {
-        nodeName = heading
+    Node(String heading, NodeType ntype) {
+        nodeName = heading;
+        nodeType = ntype;
+    }
+
+    void setPayload(String npayload) {
+        this.nodePayload = npayload
+    }
+
+    void addChildNode(Node childNode) {
+        this.subNodes.add(childNode);
+    }
+
+    void clearNodes() {
+        this.subNodes.clear();
     }
 }
