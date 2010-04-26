f44f6bfad14063fe8c3ea5fb32c72ca81907485c
Matthew Jones <mat@matburt.net>
2010-04-26 00:49:27 -0400

    Initial Widget implementation

---
+++ src/com/matburt/mobileorg/MobileOrgWidget.java
@@ -0,0 +1,57 @@
+package com.matburt.mobileorg;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.IBinder;
+import android.text.format.Time;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileOrgWidget extends AppWidgetProvider {
+   @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+            int[] appWidgetIds) {
+        // To prevent any ANR timeouts, we perform the update in a service
+        context.startService(new Intent(context,
+                                        MobileOrgWidgetService.class));
+    }
+
+    public static class MobileOrgWidgetService extends Service {
+        @Override
+        public void onStart(Intent intent, int startId) {
+            // Build the widget update for today
+            RemoteViews updateViews = this.genUpdateDisplay(this);
+            
+            // Push update for this widget to the home screen
+            ComponentName thisWidget = new ComponentName(this,
+                                                         MobileOrgWidget.class);
+            AppWidgetManager manager = AppWidgetManager.getInstance(this);
+            manager.updateAppWidget(thisWidget, updateViews);
+        }
+
+        public RemoteViews genUpdateDisplay(Context context) {
+            Resources res = context.getResources();
+            RemoteViews updateViews = null;
+            updateViews = new RemoteViews(context.getPackageName(),
+                                          R.layout.widget_mobileorg);
+            updateViews.setTextViewText(R.id.message, "Everything worked");
+            return updateViews;
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            // We don't need to bind to this service
+            return null;
+        }
+    }
+}
