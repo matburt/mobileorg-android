b6a9f349c7aa40601b89a5db2e6f7258b25059da
Matthew Jones <mat@matburt.net>
2010-05-22 22:27:04 -0400

    Initialize the settings activity

--- src/com/matburt/mobileorg/SettingsActivity.java
+++ src/com/matburt/mobileorg/SettingsActivity.java
@@ -9,6 +9,9 @@ import android.widget.Toast;
 
 public class SettingsActivity extends PreferenceActivity
 {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
 }
-
