bc0024c4f46ac899b31358277a9405b62eee270d
Matthew Jones <mat@matburt.net>
2010-09-02 20:59:51 -0400

    Initial libraries and synchronizer for Dropbox

---
+++ src/com/matburt/mobileorg/DropboxSynchronizer.java
@@ -0,0 +1,23 @@
+package com.matburt.mobileorg;
+
+import com.dropbox.client.DropboxAPI;
+import com.dropbox.client.DropboxAPI.Config;
+
+public class DropboxSynchronizer implement Synchronizer
+{
+    //Replace these with the Dropbox API key and secret
+    final static private String CONSUMER_KEY = "aaaa";
+    final static private String CONSUMER_SECRET = "zzzz";
+
+    DropboxSynchronizer(Activity parentActivity) {
+        this.rootActivity = parentActivity;
+        this.r = this.rootActivity.getResources();
+        this.appdb = new MobileOrgDatabase((Context)parentActivity);
+        this.appSettings = PreferenceManager.getDefaultSharePreferences(
+                                   parentActivity.getBaseContext());
+    }
+
+    public void close() {
+        this.appdb.close();
+    }
+}
