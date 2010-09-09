062de3756ef1237e8c031a42e1cb05ce8ac0b354
Adam Marek <adam.marek@gmail.com>
2010-09-09 13:29:23 +0200

    It looks that eliminating the per-synchronizer settings activities was a premature optimization. It makes the dispatching code unnecesarily convoluted. My bad.

---
+++ src/com/matburt/mobileorg/SDCardSettingsActivity.java
@@ -0,0 +1,17 @@
+package com.matburt.mobileorg;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.widget.Toast;
+
+public class SDCardSettingsActivity extends PreferenceActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.webdav_preferences);
+    }
+}
