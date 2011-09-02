bd00e1a406326e20d1b494f6c0676f266c852c3b
Uriel Avalos <uriavalos@yahoo.com>
2011-09-02 19:50:43 -0400

    Fri Sep 02 19:50:43 EDT 2011

--- src/com/matburt/mobileorg/Settings/WizardActivity.java
+++ src/com/matburt/mobileorg/Settings/WizardActivity.java
@@ -44,29 +44,29 @@ public class WizardActivity extends Activity
 	// }
 
 	//get ids and pointers to sync radio buttons
-	syncGroup = (RadioGroup) findViewById(R.id.sync_group);
-	syncWebDav = ( (RadioButton) 
-			findViewById(R.id.sync_webdav) ).getId();
-	syncDropBox = ( (RadioButton) 
-			findViewById(R.id.sync_dropbox) ).getId();
-	syncSdCard = ( (RadioButton) 
-			findViewById(R.id.sync_sdcard) ).getId();
-	//setup click listener for sync radio group
-	syncGroup.setOnCheckedChangeListener(this);
+	// syncGroup = (RadioGroup) findViewById(R.id.sync_group);
+	// syncWebDav = ( (RadioButton) 
+	// 		findViewById(R.id.sync_webdav) ).getId();
+	// syncDropBox = ( (RadioButton) 
+	// 		findViewById(R.id.sync_dropbox) ).getId();
+	// syncSdCard = ( (RadioButton) 
+	// 		findViewById(R.id.sync_sdcard) ).getId();
+	// //setup click listener for sync radio group
+	// syncGroup.setOnCheckedChangeListener(this);
     }
 
     @Override
     	public void onCheckedChanged(RadioGroup arg, int checkedId) {
-    	SharedPreferences appSettings = 
-    	    PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-    	SharedPreferences.Editor editor = appSettings.edit();
-    	if ( checkedId == syncWebDav )
-    	    editor.putString("syncSource", "webdav");
-    	else if ( checkedId == syncDropBox )
-    	    editor.putString("syncSource", "dropbox");
-    	else if ( checkedId == syncSdCard)
-    	    editor.putString("syncSource", "sdcard");
-    	editor.commit();
+    	// SharedPreferences appSettings = 
+    	//     PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+    	// SharedPreferences.Editor editor = appSettings.edit();
+    	// if ( checkedId == syncWebDav )
+    	//     editor.putString("syncSource", "webdav");
+    	// else if ( checkedId == syncDropBox )
+    	//     editor.putString("syncSource", "dropbox");
+    	// else if ( checkedId == syncSdCard)
+    	//     editor.putString("syncSource", "sdcard");
+    	// editor.commit();
     }
 
     public class PageView extends ScrollView {
