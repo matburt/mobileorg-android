f85aa577c5e1cc8fb2965f218fefeaa8c51d83e9
Uriel Avalos <uriavalos@yahoo.com>
2011-09-05 21:28:50 -0400

    transfer to USB

---
+++ src/com/matburt/mobileorg/Settings/PageFlipView.java
@@ -0,0 +1,108 @@
+package com.matburt.mobileorg.Settings;
+
+import android.util.Log;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Context;
+import android.view.Display;
+import android.widget.LinearLayout;
+import android.widget.HorizontalScrollView;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.util.AttributeSet;
+import android.view.WindowManager;
+import android.graphics.Canvas;
+import android.view.GestureDetector; 
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.View;
+import android.view.MotionEvent;
+import com.matburt.mobileorg.R;
+
+public class PageFlipView extends HorizontalScrollView {
+    static String TAG = "PageFlipView";
+    static final int SWIPE_MIN_DISTANCE = 5;
+    static final int SWIPE_THRESHOLD_VELOCITY = 300;
+ 
+    //private ArrayList<ArticleListItem> mItems = null;
+    GestureDetector mGestureDetector;
+    int mActiveFeature = 0;
+    LinearLayout container;
+
+    public PageFlipView(Context context) {
+	super(context);
+    }
+
+    public PageFlipView(Context context, AttributeSet attrs) {
+	super(context, attrs);
+	//get number of pages
+	container = (LinearLayout) ((Activity) getContext())
+	    .findViewById(R.id.wizard_container);
+	//Log.d(TAG,"Container count: "+container.getChildCount());
+	//setup page flips
+	//see http://blog.velir.com/index.php/2010/11/17/android-snapping-horizontal-scroll/
+	mGestureDetector = new GestureDetector(getContext(),
+					       new MyGestureDetector());
+					       //new MyGestureDetector());
+        setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //If the user swipes
+                if (mGestureDetector.onTouchEvent(event)) {
+                     return true;
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP
+		   || event.getAction() == MotionEvent.ACTION_CANCEL ){
+                    int scrollX = getScrollX();
+                    int featureWidth = v.getMeasuredWidth();
+                    mActiveFeature = ((scrollX + (featureWidth/2))/featureWidth);
+                    int scrollTo = mActiveFeature*featureWidth;
+                    smoothScrollTo(scrollTo, 0);
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        });
+    }
+
+    class MyGestureDetector extends SimpleOnGestureListener {
+        @Override
+	    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+		Log.d(TAG,"velocity:"+String.valueOf(velocityX)+
+		      " activeFeature:"+String.valueOf(mActiveFeature)+
+		      " childCount:"+String.valueOf(container.getChildCount())+
+		      " featureWidth:"+String.valueOf(getMeasuredWidth()));
+                //right to left
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+		   && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    int featureWidth = getMeasuredWidth();
+                    mActiveFeature = (mActiveFeature < (container.getChildCount() - 1)) ?
+			mActiveFeature + 1 : container.getChildCount() -1;
+                    smoothScrollTo(mActiveFeature*featureWidth, 0);
+                    return true;
+                }
+                //left to right
+                else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE 
+			 && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    int featureWidth = getMeasuredWidth();
+                    mActiveFeature = (mActiveFeature > 0) ? 
+			mActiveFeature - 1 : 0;
+                    smoothScrollTo(mActiveFeature*featureWidth, 0);
+                    return true;
+                }
+            } catch (Exception e) {
+		Log.e(TAG, "There was an error processing the Fling event:" + e.getMessage());
+            }
+            return false;
+        }
+
+	@Override  
+	    public boolean onDown(MotionEvent e) {  
+	    Log.v(TAG, "onDown");  
+	    return true;  
+	}  
+    }
+}
+
---
+++ src/com/matburt/mobileorg/Settings/PageView.java
@@ -0,0 +1,116 @@
+package com.matburt.mobileorg.Settings;
+
+import android.util.Log;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.view.Display;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.util.AttributeSet;
+import android.view.WindowManager;
+import android.graphics.Canvas;
+import com.matburt.mobileorg.R;
+
+public class PageView extends ScrollView {
+    static String TAG = "PageView";
+
+    public PageView(Context context) {
+	super(context);
+    }
+
+    public PageView(Context context, AttributeSet attrs) {
+	super(context, attrs);
+    }
+
+    @Override 
+	protected void onMeasure(int widthMeasureSpec, 
+				 int heightMeasureSpec){
+	int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+	int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+	Display display = ( (WindowManager)
+    			    getContext().getSystemService(Context.WINDOW_SERVICE) )
+    	    .getDefaultDisplay(); 
+        int screenWidth = display.getWidth();
+        int screenHeight = display.getHeight();
+	setMeasuredDimension(measureWidth(widthMeasureSpec),
+			     measureHeight(heightMeasureSpec));
+
+	//setMeasuredDimension(screenWidth,
+	//		     screenHeight);
+	setLayoutParams(new 
+			LinearLayout
+			.LayoutParams(
+				      measureWidth(widthMeasureSpec),
+				      measureHeight(heightMeasureSpec))
+			);
+	super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+    // @Override
+    //     protected void onMeasure(int widthMeasureSpec, 
+    // 				 int heightMeasureSpec) {
+    //     setMeasuredDimension(measureWidth(widthMeasureSpec),
+    // 			     measureHeight(heightMeasureSpec));
+    // }
+
+    public int measureWidth(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        Display display = ( (WindowManager)
+    			    getContext().getSystemService(Context.WINDOW_SERVICE) )
+    	    .getDefaultDisplay(); 
+        int screenWidth = display.getWidth();
+        int screenHeight = display.getHeight();
+        if (specMode == MeasureSpec.EXACTLY) {
+    	    // We were told how big to be
+    	    result = specSize;
+        } else {
+    	    // Measure the text
+    	    result = screenWidth;
+    	    if (specMode == MeasureSpec.AT_MOST) {
+    		// Respect AT_MOST value if that was what is called for by measureSpec
+    		result = Math.min(result, specSize);
+    	    }
+        }
+    	Log.d(TAG, "Width: "+String.valueOf(result));
+        return result;
+    }
+    
+    public int measureHeight(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        Display display = ( (WindowManager)
+    			    getContext().getSystemService(Context.WINDOW_SERVICE) )
+    	    .getDefaultDisplay();
+        int screenWidth = display.getWidth();
+        int screenHeight = display.getHeight();
+        if (specMode == MeasureSpec.EXACTLY) {
+    	    // We were told how big to be
+    	    result = specSize;
+        } else {
+    	    // Measure the text (beware: ascent is a negative number)
+    	    result = screenHeight;
+    	    if (specMode == MeasureSpec.AT_MOST) {
+    		// Respect AT_MOST value if that was what is called for by measureSpec
+    		result = Math.min(result, specSize);
+    	    }
+        }
+    	Log.d(TAG, "Height: "+String.valueOf(result));
+        return result;
+    }
+    
+    // @Override
+    // 	public void onDraw(Canvas canvas) {
+    // 	super.onDraw(canvas);
+    // }
+}
+
