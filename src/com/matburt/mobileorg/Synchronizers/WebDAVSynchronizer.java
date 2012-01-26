e146ad9d093b715223f85980fd0ffbe7ed8a30b5
Matthew Jones <mat@matburt.net>
2012-01-26 00:54:31 -0500

    Initial stab at supporting self-signed certs.  will expand this into a system for notifying the user and remembering certificates

--- src/com/matburt/mobileorg/Synchronizers/WebDAVSynchronizer.java
+++ src/com/matburt/mobileorg/Synchronizers/WebDAVSynchronizer.java
@@ -10,6 +10,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Pattern;
 
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
@@ -116,6 +126,28 @@ public class WebDAVSynchronizer extends Synchronizer {
 		}
 		return new BufferedReader(new InputStreamReader(mainFile));
 	}
+
+    private void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }});
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new X509TrustManager(){
+                    public void checkClientTrusted(X509Certificate[] chain,
+                                                   String authType) throws CertificateException {}
+                    public void checkServerTrusted(X509Certificate[] chain,
+                                                   String authType) throws CertificateException {}
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }}}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(
+                                                          context.getSocketFactory());
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
+    }
 
 	private DefaultHttpClient createConnection() {
 		DefaultHttpClient httpClient = new DefaultHttpClient();
