3288d075abcca98ec1c46fbf0ecfeac4c93bf3f1
Henning Weiss <hdweiss@gmail.com>
2013-09-22 11:14:55 +0200

    Converted project to gradle.

--- setup.sh
+++
@@ -1,7 +0,0 @@
-git submodule init
-git submodule update
-
-android update project -s --path . --library libs/ActionBarSherlock/library/ -l libs/locale/
-
-android update lib-project --path libs/locale/
-android update lib-project --target android-15 --path libs/ActionBarSherlock/library/
